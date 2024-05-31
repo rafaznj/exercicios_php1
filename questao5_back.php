ec36d6db00f6cf9159fb0bbb9131cfa5e7320bec
Rafael Sena <163608557+rafaznj@users.noreply.github.com>
2024-05-31 18:36:23 -0300

    2.0

--- questao5_back.php
+++ questao5_back.php
@@ -8,7 +8,28 @@
 <body>
     <?php 
     
-    
+    $num1 = $_POST["txtNumero1"];
+    $num2 = $_POST["txtNumero2"];
+
+    if ($num1 > $num2){
+        for ($i = $num1; $i <= $num2; $i++){
+            if ($i % 2 == 0){
+                echo $i;
+            }
+        }
+    }
+
+    else if ($num2 > $num1){
+        for ($i = $num2; $i <= $num1; $i++){
+            if ($i % 2 == 0){
+                echo $i;
+            }
+        }
+    }
+
+    else{
+        echo "<h1>Não há números pares entre eles.</h1>"
+    }
     
     
     
